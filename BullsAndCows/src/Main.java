c4a2f7aabfa3b1f80fb05bc78ac2f2b9b1ea9672
klOmzy <klOmzyy@gmail.com>
2024-11-25 19:45:14 +0600

    commit

--- BullsAndCows/src/Main.java
+++ BullsAndCows/src/Main.java
@@ -1,65 +1,18 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean repeat = repeatNumbersCheck();
-        System.out.println("Загадайте четыре цифры");
-        Map<Integer, Integer> hashMap1 = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < 4; i++) {
-            int number = scanner.nextInt();
-            if (number < 10) {
-                hashMap1.put(i, number);
-                set.add(number);
-            } else {
-                System.out.println("Вводить необходимо цифру! Попробуйте еще раз!");
-                return;
-            }
-        }
-        if (!repeat && set.size() < 4) {
-            System.out.println("Цифры не должны повторяться! Попробуйте еще раз!");
-            return;
-        }
-        for (int i = 0; i < hashMap1.size(); i++) {
-            System.out.print(hashMap1.get(i));
-        }
-        System.out.println();
-        System.out.println("Угадайте четыре цифры");
-        Map<Integer, Integer> hashMap2 = new HashMap<>();
-        for (int i = 0; i < 4; i++) {
-            int number = scanner.nextInt();
-            if (number < 10) {
-                hashMap2.put(i, number);
-            } else {
-                System.out.println("Вводить необходимо цифру! Попробуйте еще раз!");
-                return;
-            }
-        }
-        for (int i = 0; i < hashMap2.size(); i++) {
-            System.out.print(hashMap2.get(i));
-        }
-        System.out.println();
+    private static Map<Integer, Integer> hashMap1 = new HashMap<>();
+    private static Map<Integer, Integer> hashMap2 = new HashMap<>();
+    private static Scanner scanner = new Scanner(System.in);
+    private static int lengthNumber;
 
-        int sum1 = 0;
-        for (int i = 0; i < 4; i++) {
-            if (hashMap1.containsValue(hashMap2.get(i)) && !hashMap1.get(i).equals(hashMap2.get(i))) {
-                sum1 = sum1 + 1;
-            }
-        }
-        int sum2 = 0;
-        for (int i = 0; i < 4; i++) {
-            if (hashMap1.get(i).equals(hashMap2.get(i))) {
-                sum2 = sum2 + 1;
-            }
-        }
-        System.out.println("Результат: найдено " + sum1 + " коровы и " + sum2 + " быка");
-        if(sum2 == 4){
-            System.out.println("Вы победили!");
-        }
+    public static void main(String[] args) {
+        boolean repeat = repeatNumbersCheck();
+        writeNumbers(repeat);
+        guessNumbers();
     }
-    public static boolean repeatNumbersCheck(){
-        Scanner scanner = new Scanner(System.in);
+
+    public static boolean repeatNumbersCheck() {
         while (true) {
             System.out.println("Хотите включить повторяющиеся цифры? YES OR NO");
             String repeatCheck = scanner.nextLine();
@@ -70,6 +23,73 @@ public class Main {
             } else {
                 System.out.println("Попробуйте еще раз!");
             }
+        }
+    }
+
+    public static void writeNumbers(boolean repeatStateCheck) {
+        System.out.println("Введите желаемую длину исходного числа");
+        lengthNumber = scanner.nextInt();
+        System.out.println("Загадайте " + lengthNumber + " цифры");
+        Set<Integer> set = new HashSet<>();
+        while (true) {
+            for (int i = 0; i < lengthNumber; i++) {
+                int number = scanner.nextInt();
+                if (number < 10) {
+                    hashMap1.put(i, number);
+                    set.add(number);
+                } else {
+                    System.out.println("Вводить необходимо цифру! Попробуйте еще раз!");
+                    writeNumbers(repeatStateCheck);
+                    return;
+                }
+            }
+            if (!repeatStateCheck && set.size() < lengthNumber) {
+                System.out.println("Цифры не должны повторяться! Попробуйте еще раз!");
+                continue;
+            }
+            break;
+        }
+    }
+
+    public static void guessNumbers() {
+        System.out.println("Угадайте четыре цифры");
+        while (true) {
+            for (int i = 0; i < lengthNumber; i++) {
+                int number = scanner.nextInt();
+                if (number < 10) {
+                    hashMap2.put(i, number);
+                } else {
+                    System.out.println("Вводить необходимо цифру! Попробуйте еще раз!");
+                    guessNumbers();
+                    return;
+                }
+            }
+            break;
+        }
+        checkResult();
+    }
+
+    public static void checkResult() {
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < lengthNumber; i++) {
+            if (hashMap1.containsValue(hashMap2.get(i)) && !hashMap1.get(i).equals(hashMap2.get(i))) {
+                sum1 = sum1 + 1;
+            }
+        }
+
+        for (int i = 0; i < lengthNumber; i++) {
+            if (hashMap1.get(i).equals(hashMap2.get(i))) {
+                sum2 = sum2 + 1;
+            }
+        }
+
+        System.out.println("Результат: найдено " + sum1 + " коровы и " + sum2 + " быка");
+        if (sum2 == lengthNumber) {
+            System.out.println("Вы победили!");
+        } else {
+            guessNumbers();
         }
     }
 }
