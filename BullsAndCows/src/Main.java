3c258e2c707503cb9126f6b24920855ecb6de95d
klOmzy <klOmzyy@gmail.com>
2024-11-26 21:31:44 +0600

    commit

--- BullsAndCows/src/Main.java
+++ BullsAndCows/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
@@ -8,10 +11,25 @@ public class Main {
     private static boolean repeat = repeatNumbersCheck();
 
     public static void main(String[] args) throws InterruptedException {
-        writeNumbers(repeat);
+        numberOptCheck();
         guessNumbers(repeat);
     }
-
+    public static void numberOptCheck(){
+        System.out.println("Хотите загадать цифры с консоли (1) или считать с файла (2). Введите 1 или 2");
+        boolean numbersOptCheck = false;
+        while (!numbersOptCheck) {
+            int numbersOpt = scanner.nextInt();
+            if(numbersOpt==1){
+                writeNumbers(repeat);
+                numbersOptCheck = true;
+            } else if (numbersOpt==2) {
+                readNumbers(repeat);
+                numbersOptCheck = true;
+            } else {
+                System.out.println("Необходимо вводить 1 или 2");
+            }
+        }
+    }
     public static boolean repeatNumbersCheck() {
         while (true) {
             System.out.println("Хотите включить повторяющиеся цифры? YES OR NO");
@@ -25,7 +43,6 @@ public class Main {
             }
         }
     }
-
     public static void writeNumbers(boolean repeatStateCheck) {
         System.out.println("Введите желаемую длину исходного числа");
         lengthNumber = scanner.nextInt();
@@ -55,7 +72,6 @@ public class Main {
             }
         }
     }
-
     public static void guessNumbers(boolean repeatStateCheck) throws InterruptedException {
         System.out.println("Угадайте " + lengthNumber + " цифры");
         Set<Integer> set = new HashSet<>();
@@ -84,7 +100,44 @@ public class Main {
         }
         checkResult();
     }
-
+    public static void readNumbers(boolean repeatStateCheck){
+        String path = "BullsAndCows/numbers.txt";
+        boolean checkNumbers = true;
+        Set<Integer> set = new HashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            int mapKey = 0;
+            while ((line = reader.readLine()) != null) {
+                int number = Integer.parseInt(line);
+                int key = mapKey++;
+                hashMap1.put(key, number);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lengthNumber = hashMap1.size();
+        for (int i = 0; i < lengthNumber; i++) {
+            if (hashMap1.get(i) < 10) {
+                set.add(hashMap1.get(i));
+            } else {
+                System.out.println("Вводить необходимо цифру! Измените исходный файл!");
+                hashMap1.clear();
+                set.clear();
+                checkNumbers = false;
+            }
+        }
+        if(hashMap1.size()==lengthNumber) {
+            if (!repeatStateCheck && set.size() < lengthNumber) {
+                System.out.println("Цифры не должны повторяться! Измените исходный файл!");
+                hashMap1.clear();
+                set.clear();
+                checkNumbers = false;
+            }
+        }
+        if(!checkNumbers){
+            numberOptCheck();
+        }
+    }
     public static void checkResult() throws InterruptedException {
         int sum1 = 0;
         int sum2 = 0;
