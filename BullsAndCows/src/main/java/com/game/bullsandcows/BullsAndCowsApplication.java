373469fd3d794dd4b231d8e82b89c1bcc2c0929b
klOmzy <klOmzyy@gmail.com>
2024-12-09 19:26:07 +0600

    removed unwanted folders

--- BullsAndCows/src/main/java/com/game/bullsandcows/BullsAndCowsApplication.java
+++
@@ -1,8 +0,0 @@
-package com.game.bullsandcows;
-
-public class BullsAndCowsApplication {
-    public static void main(String[] args) throws InterruptedException {
-        GameControl gameControl = GameControl.getInstance();
-        gameControl.startGame();
-    }
-}
